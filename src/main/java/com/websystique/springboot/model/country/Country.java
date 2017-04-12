864dbaf99484f1fc57c2f9158a00b9661ab6ba03
ecjapi <ermir.cjapi@catalysts.cc>
2017-04-12 10:44:45 +0200

    country model update

--- src/main/java/com/websystique/springboot/model/country/Country.java
+++ src/main/java/com/websystique/springboot/model/country/Country.java
@@ -1,9 +1,9 @@
 package com.websystique.springboot.model.country;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.websystique.springboot.model.country.enums.Continent;
+import com.websystique.springboot.model.country.enums.Official;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
@@ -19,6 +19,27 @@ public class Country implements Serializable {
     @Column(name = "Name")
     private String name;
 
+    @Enumerated(EnumType.STRING)
+    private Continent continent;
+
+    @Column(name = "Region")
+    private String region;
+
+    @Column(name = "IndepYear")
+    private Integer indepYear;
+
+    @Column(name = "Population")
+    private Integer population;
+
+    @Column(name = "SurfaceArea")
+    private Double surfaceArea;
+
+    @Column(name = "GNP")
+    private Double gnp;
+
+    @Column(name = "LocalName")
+    private String localName;
+
     public String getCode() {
         return code;
     }
@@ -33,5 +54,61 @@ public class Country implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Continent getContinent() {
+        return continent;
+    }
+
+    public void setContinent(Continent continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public Integer getIndepYear() {
+        return indepYear;
+    }
+
+    public void setIndepYear(Integer indepYear) {
+        this.indepYear = indepYear;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public Double getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public void setSurfaceArea(Double surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public Double getGnp() {
+        return gnp;
+    }
+
+    public void setGnp(Double gnp) {
+        this.gnp = gnp;
+    }
+
+    public String getLocalName() {
+        return localName;
+    }
+
+    public void setLocalName(String localName) {
+        this.localName = localName;
     }
 }
